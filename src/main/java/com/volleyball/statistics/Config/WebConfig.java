a61a15dee460fa40cb094caf4890e7ece2030fea
DanielRuiz <danimail952@gmail.com>
2024-12-18 16:24:15 +0100

    Added config to allow calling the endpoints from Angular local frontend

---
+++ src/main/java/com/volleyball/statistics/Config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.volleyball.statistics.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    // TODO check how can I do this when deployed to prod
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
